d4f7dde63d84a497059669e585652afab4a6874b
parsaabadi <47127435+parsaabadi@users.noreply.github.com>
2019-02-15 23:23:16 -0500

    Update project_analyze.sh
    committed updated code part 1

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,1 +1,20 @@
-##empty
+##updated version
+#!/bin/bash
+
+rm ./todo.log
+grep -rin "#TODO" . | grep -v todo.log > /tmp/todo.log
+mv /tmp/todo.log ./
+
+FILES=".html:HTML
+.js:JavaScript
+.css:CSS
+.py:Python
+.hs:Haskell
+.sh:Bash"
+
+for line in $FILES; do
+  ext=`echo $line | cut -d: -f1`
+  name=`echo $line | cut -d: -f2`
+  count=`find . -type f | grep "$ext" | wc -l`
+  echo "$name: $count"
+done
